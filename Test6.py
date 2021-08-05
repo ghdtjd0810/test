a4e083f8e84d8834f2dbfb6f7a703405bec0bffc
ghdtjd0810 <88218425+ghdtjd0810@users.noreply.github.com>
2021-08-05 14:11:53 +0900

    Update Test6.py

--- Test6.py
+++ Test6.py
@@ -1,3 +1,4 @@
+'''
 class Rectangle:
     def __init__(self, side=0, num =0):
         self.side = side
@@ -17,22 +18,27 @@ a = Rectangle()
 count = 5
 printAreas(a, count)
 
-
-class radius:
-    def __init__(self, r):
-        self.r = r
-        
-
+class Circle:
+    def __init__(self, radius):# 초기화 하려면  r에다가 10을 넣으면 될 듯
+        self.radius = radius
+    def setRadius(self,r):
+        self.radius = r
+    def getRadius(self):
+        return self.radius
+    
     def wide(self):
-        
-        return 3.14*self.r*self.r
+        area = 3.14*self.radius * self.radius
+        return area
 
     def side(self):
-        return 2*3.14*self.r
-a = radius(4)
+        return 2*3.14*self.radius
+
+a = Circle(4)
 
 print(a.wide())
 print(a.side())
+
+
 
 
 class Balance():
@@ -49,6 +55,33 @@ a.withdraw()
 a.deposit()
 
 
+class Bank:
+    def __init__(self, balance = 0): # 초기화가 있으면 setbalance 하는게 좋다하시는데?
+        self.balance = balance
+    def setBalance(self, b):
+        self.balance = b
+    def getBalance(self):
+        return self.balance
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print('출금이 안됌')
+        else:
+            self.balance -= amount
+            print('통장에서', self.balance, '만큼 출금됌')
+
+    def deposit(self, amount):
+        self.balance += amount
+        print('잔액', self.balance)
+
+a = Bank(0)#뺑크 하나 만들고 초기값 설정
+a.deposit(10)# 입금하면서 10 느는건데, amount = 10으로 함수 받음
+a.deposit(20)
+a.withdraw(5)# 얘도 balance에다가 5를 출금 하는 형태, 즉 amount르ㅗ 함수 받음
+
+'''
+        
+# 내 
 class Cat():
     def __init__(self, name, age):
         self.name = name
@@ -59,7 +92,26 @@ class Cat():
         print(self.age)
 
 a = Cat('Missy', 3)
+b = Cat('Lucky', 5)
+
 a.call()
+b.call()
+
+# 여기는 교수님 해답 
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def setName(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def setAge(self,age):
+        self.age = age
+    def getAge(self):
+        return self.age
+
+
 class Box():
     def __init__(self, width, height, depth):
         self.width = width
